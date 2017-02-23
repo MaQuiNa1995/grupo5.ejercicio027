6cec229777106d118223bdfda7ada3af271dcd27
curso18 <curso18java@gmail.com>
2017-02-23 21:12:44 +0100

    Ultima version antes de cenar clases UI estructuradas de 2 formas 1 es lenta y no carga los datos y el otro localhost:8080/prueba2 da error al hacer click al boton esta ultima la copie todo deprisa estoy cansado en principio era una prueba haber porque no me cargaba pero la dejo por si interesa esa estructura aunque es anarquica por los nombres de variables

--- src/main/java/es/cic/curso/grupo5/ejercicio027/frontend/principal/FormPrincipal.java
+++ src/main/java/es/cic/curso/grupo5/ejercicio027/frontend/principal/FormPrincipal.java
@@ -10,6 +10,9 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+import es.cic.curso.grupo5.ejercicio027.frontend.secundarios.GridDummie;
 
 @Theme("myTheme")
 public class FormPrincipal extends UI {
@@ -22,30 +25,28 @@ public class FormPrincipal extends UI {
 	@Override
 	protected void init(VaadinRequest request) {
 		
-		final HorizontalLayout panelTodo = new HorizontalLayout();
+		final VerticalLayout panelTodo = new VerticalLayout();
 		panelTodo.setMargin(true);
 		panelTodo.setSpacing(true);
 		panelTodo.setWidth("100%");
 		
 		Label labelDummie = new Label("Prueba Dummie De La Puesta a Punto De Vaadin");
-		
-		/**
-		 * final VerticalLayout gridFiguras = new GridFiguras();
-		 */
-		
-//		final VerticalLayout annadirFiguras = new AnnadirFiguras();
-//		final VerticalLayout eliminarFiguras = new EliminarFiguras();
-//		final VerticalLayout actualizarFiguras = new ActualizarFiguras();
-//		final VerticalLayout listarFiguras = new ListarFiguras();
 
-//		TabSheet menu = new TabSheet();
-//        menu.setHeight(100.0f, Unit.PERCENTAGE);
-//        menu.addTab(, "Añadir Figuras");
-//        menu.addTab(, "Eliminar Figuras");
-//        menu.addTab(, "Actualizar Figuras");
-//        menu.addTab(, "listar Figuras");
+		final VerticalLayout gridFiguras = new GridDummie();
+
+//		final VerticalLayout annadir = new AnnadirDummie();
+//		final VerticalLayout eliminar = new EliminarDummie();
+//		final VerticalLayout actualizar = new ActualizarDummie();
+//		final VerticalLayout listar = new ListarDummie();
+
+		TabSheet menu = new TabSheet();
+        menu.setHeight(100.0f, Unit.PERCENTAGE);
+        menu.addTab(gridFiguras, "Añadir Dummie");
+        menu.addTab(gridFiguras, "Eliminar Dummie");
+        menu.addTab(gridFiguras, "Actualizar Dummie");
+        menu.addTab(gridFiguras, "Listar Dummie");
 	
-		panelTodo.addComponent(labelDummie);
+		panelTodo.addComponents(menu,labelDummie,gridFiguras);
 		
 		setContent(panelTodo);
 	}
---
+++ src/main/java/es/cic/curso/grupo5/ejercicio027/frontend/principal/FormPrincipal2.java
@@ -0,0 +1,117 @@
+package es.cic.curso.grupo5.ejercicio027.frontend.principal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+import es.cic.curso.grupo5.ejercicio027.backend.dominio.Dummie;
+import es.cic.curso.grupo5.ejercicio027.backend.service.ClaseDummieService;
+import es.cic.curso.grupo5.ejercicio027.frontend.secundarios.DummieForm;
+
+@Theme("mytheme")
+public class FormPrincipal2 extends UI {
+    
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -199465035744553464L;
+	private Grid maestro;
+	private DummieForm detalle;
+	private List<Dummie> listaPeliculas;
+	
+
+	@Autowired
+	private ClaseDummieService peliculaService;
+	
+	private Button accionAniadir;
+	@Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        listaPeliculas = new ArrayList<>();
+        listaPeliculas.add(new Dummie("PruebaPatatas", true, 777, 9127487528l, 1857f));
+        listaPeliculas.add(new Dummie("PruebaPatatas2", true, 777, 9127487528l, 1857f));
+        
+        maestro = new Grid();
+        maestro.setColumns("palabra","apagado","numero","numeroGran","numeroComas");
+        
+        cargaGrid();
+        
+        maestro.addSelectionListener(e -> 
+        	{
+        		Dummie p = null;
+        		if (!e.getSelected().isEmpty() ) {
+	        		p = (Dummie) e.getSelected().iterator().next();
+        		} 
+        		detalle.setDummie(p);
+        	});
+        
+        detalle = new DummieForm(this);
+        
+        accionAniadir = new Button("Añadir");
+		
+		accionAniadir.addClickListener(e -> aniadirGrid());
+		
+		
+        
+        layout.addComponents(accionAniadir, maestro, detalle );
+        layout.setMargin(true);
+        layout.setSpacing(true);
+       
+        
+        setContent(layout);
+    }
+
+	public void cargaGrid() {
+		maestro.setContainerDataSource(
+        		new BeanItemContainer<>(Dummie.class, listaPeliculas)
+        );
+	}
+	
+	public void aniadirGrid() {
+		
+		Dummie p = new Dummie("PruebaPatatas", true, 777, 9127487528l, 1857f);
+		detalle.setDummie(p);
+		peliculaService.aniadirClaseDummie("PruebaPatatas", true, 777, 9127487528l, 1857f);
+		listaPeliculas.add(p);
+		maestro.setContainerDataSource(
+				
+				new BeanItemContainer<>(Dummie.class, listaPeliculas)
+        		
+        );
+	}
+	
+	public void borrarGrid(Dummie pelicula) {
+		
+		listaPeliculas.remove(pelicula);
+		maestro.setContainerDataSource(
+				new BeanItemContainer<>(Dummie.class, listaPeliculas)
+        		
+        );
+		
+		
+	}
+	
+	@WebServlet(value = "/prueba2", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = FormPrincipal2.class)
+	public static class Servlet extends VaadinServlet {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -796390695740401802L;
+	}
+}

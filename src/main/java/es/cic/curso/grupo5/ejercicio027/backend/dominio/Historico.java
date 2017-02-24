91330a1bd326ab52946742843e62d058a2b97d5c
curso18 <curso18java@gmail.com>
2017-02-24 11:41:30 +0100

    creacion de las clases usuario e historico bien metidas y eliminacion de foto prueba de vaadin

---
+++ src/main/java/es/cic/curso/grupo5/ejercicio027/backend/dominio/Historico.java
@@ -0,0 +1,129 @@
+package es.cic.curso.grupo5.ejercicio027.backend.dominio;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import es.cic.curso.grupo5.ejercicio027.backend.repository.Identificable;
+
+@Entity
+public class Historico implements Identificable<Long>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3600623485273463731L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+	
+	@Column(name ="operacion")
+	String operacion;
+	
+	@Column(name ="hora")
+	String hora;
+	
+    @JoinColumn(name = "id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Usuario usuario;
+    
+	public Historico() {
+		super();
+	}
+
+	public Historico(Long id, String operacion, String hora, Usuario usuario) {
+		super();
+		this.id = id;
+		this.operacion = operacion;
+		this.hora = hora;
+		this.usuario = usuario;
+	}
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id=id;
+	}
+
+	public String getOperacion() {
+		return operacion;
+	}
+
+	public void setOperacion(String operacion) {
+		this.operacion = operacion;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((hora == null) ? 0 : hora.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((operacion == null) ? 0 : operacion.hashCode());
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Historico other = (Historico) obj;
+		if (hora == null) {
+			if (other.hora != null)
+				return false;
+		} else if (!hora.equals(other.hora))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (operacion == null) {
+			if (other.operacion != null)
+				return false;
+		} else if (!operacion.equals(other.operacion))
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
+	
+	
---
+++ src/main/java/es/cic/curso/grupo5/ejercicio027/backend/dominio/Usuario.java
@@ -0,0 +1,140 @@
+package es.cic.curso.grupo5.ejercicio027.backend.dominio;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import es.cic.curso.grupo5.ejercicio027.backend.repository.Identificable;
+
+@Entity
+public class Usuario implements Identificable<Long>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7475082818754761276L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+	
+	@Column(name ="tipo")
+    String nombre;
+	
+	@Column(name ="password")
+    String password;
+    
+	@Column(name ="rol")
+    String rol;
+    
+	@Column(name ="email")
+    String email;
+
+	public Usuario() {
+		super();
+	}
+	
+	public Usuario(Long id, String nombre, String password, String rol, String email) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.password = password;
+		this.rol = rol;
+		this.email = email;
+	}
+	
+	@Override
+	public Long getId() {
+		return this.id;
+	}
+	@Override
+	public void setId(Long id) {
+		this.id=id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((rol == null) ? 0 : rol.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (rol == null) {
+			if (other.rol != null)
+				return false;
+		} else if (!rol.equals(other.rol))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
